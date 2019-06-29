191afc25c19ddf15b4f56df629a1d6101752772e
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-29 01:02:40 -0500

    displaying a single venue name

--- liri.js
+++ liri.js
@@ -1,9 +1,12 @@
+// requires
 require("dotenv").config()
 var fs = require("fs")
 var axios = require("axios");
 var moment = require("moment");
 var spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+
+// global variables
 var spotify = new spotify(keys.spotify);
 var argv2 = process.argv[2]
 var omdbapi = "bbc6c707";
@@ -12,11 +15,13 @@ var artist = artistInput.join(" ");
 
 var bandsInTown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
+// concert grabbing from Bands in Town api
 if (argv2 === "concert-this") {
     axios.get(bandsInTown).then(
         function (response) {
-
-            // console.log("Venue: " + response.data);
+            // for(var val in response){
+            console.log(response.data[0].venue.name);
+            // }
         }
     )
 }
