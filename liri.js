076d7deaa4069d887f45d6523c14289de9ef1729
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-06-29 00:23:55 -0500

    moved the require dontenv to the top.  added require fs, added var keys, spotify, argv2, artistInput, artist, bandsInTown, and the if statement for concert-this.  However, it's not showing as it should in the console log.  moved where I stored var omdbapi

--- liri.js
+++ liri.js
@@ -1,8 +1,27 @@
+require("dotenv").config()
+var fs = requrie("fs")
 var axios = require("axios");
 var moment = require("moment");
 var spotify = require("node-spotify-api");
-var dontenv = require("dotenv").config()
+var keys = require("./keys.js");
+var spotify = new spotify(keys.spotify);
+var argv2 = process.argv[2]
+var omdbapi = "bbc6c707";
+var artistInput = process.argv.slice(3);
+var artist = artistInput.join(" ");
+
+var bandsInTown = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+if (argv2 === "concert-this") {
+    axios.get(bandsInTown).then(
+        function (response) {
+
+            // console.log("Venue: " + response.data);
+        }
+    )
+}
 
 
-
-var omdbapi = bbc6c707;
+// Name of the venue
+// Venue location
+// Date of the Event(use moment to format this as "MM/DD/YYYY")
